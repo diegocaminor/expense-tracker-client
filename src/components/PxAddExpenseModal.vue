4625e27acab6ef9e99ef86c42dabd1cbe48a729d
Diego Camino <diego.camino.r@gmail.com>
2020-06-08 19:22:44 -0500

    Add expense form validations

--- src/components/PxAddExpenseModal.vue
+++ src/components/PxAddExpenseModal.vue
@@ -12,17 +12,19 @@
       <div class="modal-content">
         <div class="modal-header">
           <h5 class="modal-title" id="addExpenseModalLabel">Nuevo gasto</h5>
-          <button
-            type="button"
-            class="close"
-            data-dismiss="modal"
-            aria-label="Close"
-          >
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
             <span aria-hidden="true">&times;</span>
           </button>
         </div>
         <div class="modal-body">
           <form>
+            <div class="form-group">
+              <div v-if="errors.length">
+                <div class="text-left alert alert-danger" role="alert">
+                  <p v-for="error in errors" :key="error">{{ error }}</p>
+                </div>
+              </div>
+            </div>
             <div class="form-group">
               <input
                 type="number"
@@ -33,13 +35,12 @@
             </div>
             <div class="form-group dropdown">
               <select v-model="expense.category" class="form-control">
-                <option disabled selected="selected" value="">Category</option>
+                <option disabled selected="selected" value>Category</option>
                 <option
                   v-for="category in categories"
                   :key="category.id"
                   :value="category"
-                  >{{ category.name }}</option
-                >
+                >{{ category.name }}</option>
               </select>
             </div>
             <div class="form-group">
@@ -55,12 +56,8 @@
           </form>
         </div>
         <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-dismiss="modal">
-            Close
-          </button>
-          <button @click="addExpense" type="button" class="btn btn-primary">
-            Save changes
-          </button>
+          <button id="closeModal" type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+          <button @click="addExpense" type="button" class="btn btn-primary">Save changes</button>
         </div>
       </div>
     </div>
@@ -71,7 +68,8 @@
 import api from "@/api.js";
 
 class Expense {
-  constructor(amount, categoryId, categoryName, notes) {
+  constructor(userId, amount, categoryId, categoryName, notes) {
+    this.userId = userId || "5ed9f0c97745d7515f0910b0";
     this.amount = amount || 0;
     this.category = categoryId || "";
     this.notes = notes || "";
@@ -87,26 +85,40 @@ export default {
       categories: [
         {
           categoryId: "5ed878eb1863db2e8822633b",
-          name: "Home",
+          name: "Home"
         },
         {
           categoryId: "5ed878eb1863db2e88f26332",
-          name: "Pets",
+          name: "Pets"
         },
         {
           categoryId: "5ed878eb1863db2e88f26334",
-          name: "House",
-        },
+          name: "House"
+        }
       ],
       requestMessage: "",
+      errors: []
     };
   },
   methods: {
     async addExpense() {
-      this.requestMessage = await api.addExpense(this.expense);
-      alert(this.requestMessage);
-      this.expense = new Expense();
-    },
-  },
+      this.errors = [];
+      if (!this.expense.amount || this.expense.amount == 0) {
+        this.errors.push("Amount required");
+      }
+      if (!this.expense.category) {
+        this.errors.push("Category required");
+      }
+      if (!this.expense.notes) {
+        this.errors.push("Notes required");
+      }
+      if (!this.errors.length) {
+        this.requestMessage = await api.addExpense(this.expense);
+        alert(this.requestMessage);
+        this.expense = new Expense();
+        document.getElementById("closeModal").click();
+      }
+    }
+  }
 };
 </script>
